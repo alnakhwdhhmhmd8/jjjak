8f9d5e4b27fad1842a17ca2ff0237e356b3da0b0
alnakhwdhhmhmd8 <alnakhwdhhmhmd8@gmail.com>
2025-08-09 12:50:13 +0300

    Uploaded Successfully

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["wwvvwl"]
+OWNER_NAME = "𝐃𝐄𝐕 ᴊᴀᴋلـ 公"
+BOT_TOKEN = "7761920049:AAGs18loNT_3VbXLYvrfe3OSdTSFUOrmado"
+DATABASE = "mongodb+srv://mhmdalymany935:aHXo8qymo04IeJIg@jak.neur7n5.mongodb.net/?retryWrites=true&w=majority&appName=jak"
+CHANNEL = "https://t.me/xvwwvl"
+GROUP = "https://t.me/xvwwvl"
+VID_SO = "https://graph.org/file/269990d00297ddd118209.jpg"
+PHOTO = "https://graph.org/file/269990d00297ddd118209.jpg"
+LOGS = "Dve_jak"
